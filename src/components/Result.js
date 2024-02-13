61c223019bf4a3303856610781454bfcbb3f6d3d
loveravocado <makaron-ayako@docomo.ne.jp>
2024-02-13 14:03:57 +0900

    result画面修正

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import backimg from '../img/backimg.png';
+import backimg from '../img/backresult.png';
+import peppermint from '../img/peppermint.png';
+import editimg from '../img/edit.png';
 import './Result.css';
 
 export default function Result() {
   return (
     <>
         <div className='back'>
-            <div className="border">
-                <div className="backimg">
-                    <img  className="img" src={backimg} alt="back" />   
+            <div className="backimg"><img  className="img" src={backimg} alt="back" /></div>
+            <div className="topname">Perfume Pairing</div>
+            <div className="resultall">
+                <div className="resultdetail">
+                    <img src={peppermint} className='resultitemimg' alt='img'></img>
+                    <div className='resultitemname'>ペパーミント</div>
+                    <img src={editimg} className='resulteditimg' alt='img'></img>
                 </div>
-                <div className="topname">Perfume Pairing</div>
             </div>
         </div>
     </>
