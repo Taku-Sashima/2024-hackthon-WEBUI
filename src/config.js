836abf50042840afd65a143876c076dadf30755d
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-02-23 17:15:41 +0900

    Update raw_host variable to use process.env.API_HOST

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
-
-
 //接続したいPCのIPアドレスに変える
-const raw_host = "localhost:8000"
-
-
+const raw_host = process.env.API_HOST || 'localhost:8000'
 
 export const host = `http://${raw_host}`
 export const ws_host = `ws://${raw_host}`
