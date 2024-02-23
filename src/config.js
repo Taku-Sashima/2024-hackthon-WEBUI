d1b4727c94b4b8a75b7d7c09b73dc9d371cbf09a
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-02-23 17:25:25 +0900

    Update API host configuration

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //接続したいPCのIPアドレスに変える
-const raw_host = process.env.API_HOST || 'localhost:8000'
+const raw_host = process.env.REACT_APP_API_HOST || 'localhost:8000'
 
 export const host = `http://${raw_host}`
 export const ws_host = `ws://${raw_host}`
