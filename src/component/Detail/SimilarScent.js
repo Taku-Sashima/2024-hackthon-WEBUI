76bfb85f9f602094e59ad2175dec61216c49ff72
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-02-23 15:26:31 +0900

    Refactor SimilarScent component and Detail page, to fit the new API

--- src/component/Detail/SimilarScent.js
+++ src/component/Detail/SimilarScent.js
@@ -2,22 +2,31 @@ import React from 'react'
 
 //props以下を擬似データのものに変更する
 
-export default function SimilarScent(props){
-
-  return(
+export default function SimilarScent(props) {
+  return (
     <div className="item">
       <div className="itemdetail">
-          <div className='imgname'>
-              <img src= {"/img/ranking/number" + `${Number(props.listIndex)+1 }` + ".png"} className='numberimg' alt='number1'></img>
-              <div className='itemname'>{props.details.item_name}</div>
+        <div className="imgname">
+          <img
+            src={'/img/ranking/number' + `${Number(props.listIndex) + 1}` + '.png'}
+            className="numberimg"
+            alt="number1"
+          ></img>
+          <div className="itemname">{props.details.item_name}</div>
+        </div>
+        <img
+          src={`/img/item/item${props.details.img_url}.png`}
+          className="itemimg"
+          alt={'item_img' + props.details.id}
+        ></img>
+        <div className="details">
+          <div className="feature">
+            {props.details.item_tags[0]},{props.details.item_tags[1]}
           </div>
-          <img src={"/img/item/item" + `${Number(props.listIndex)+1 }` + ".png"} className='itemimg' alt={'item_img' + props.details.id}></img>
-          <div className="details">
-              <div className="feature">{props.details.item_tags[0]},{props.details.item_tags[1]}</div>
-              <div className="class">{props.details.product_label}</div>
-          </div>
-          <div className="similarity">{props.details.similarity*100}%</div>
+          <div className="class">{props.details.product_label}</div>
+        </div>
+        <div className="similarity">{Math.floor(props.details.similarity * 100, 2)}%</div>
       </div>
-  </div>
+    </div>
   )
 }
